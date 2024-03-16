d7c69fc80c69881c50f83873b805ebefd529290a
Rafael Yudi Shirakura <112785421+TentandoSorteComJava@users.noreply.github.com>
2024-03-16 09:19:29 -0300

    Front mudado

--- Front/js/script.js
+++ Front/js/script.js
@@ -4,18 +4,20 @@ document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
 }
 
+
 let account = document.querySelector('.user-account');
 
 document.querySelector('#user-btn').onclick = () =>{
-   account.classList.add('active');
+   document.location.href="pginicial.html";   
 }
-
+/*
 document.querySelector('#close-account').onclick = () =>{
    account.classList.remove('active');
 }
+*/
 
 let myOrders = document.querySelector('.my-orders');
-
+/*
 document.querySelector('#order-btn').onclick = () =>{
    myOrders.classList.add('active');
 }
@@ -23,9 +25,11 @@ document.querySelector('#order-btn').onclick = () =>{
 document.querySelector('#close-orders').onclick = () =>{
    myOrders.classList.remove('active');
 }
+*/
 
 let cart = document.querySelector('.shopping-cart');
 
+/*
 document.querySelector('#cart-btn').onclick = () =>{
    cart.classList.add('active');
 }
@@ -33,6 +37,7 @@ document.querySelector('#cart-btn').onclick = () =>{
 document.querySelector('#close-cart').onclick = () =>{
    cart.classList.remove('active');
 }
+*/
 
 window.onscroll = () =>{
    navbar.classList.remove('active');
